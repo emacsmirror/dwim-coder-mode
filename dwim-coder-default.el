29ba9b32d0084d7e025f7f789f5b60dfd2d4fc88
Mohammed Sadiq <sadiq@sadiqpk.org>
2023-07-27 19:36:51 +0530

    default: Use the common implementation for ;
    So that ; behaves the same in every modes

--- dwim-coder-default.el
+++ dwim-coder-default.el
@@ -104,48 +104,10 @@
     (insert "...")
     t)))
 
-(defun dwim-coder-default-dwim-semi-colon ()
-  (let ((value nil))
-    (cond
-     ;; goto end of string if inside one
-     ((nth 3 (syntax-ppss))
-      (skip-syntax-forward "^\"")
-      (forward-char)
-      t)
-     ;; On empty lines, delete the line and go to the end of last line
-     ((save-excursion (beginning-of-line)
-                      (looking-at-p "^ *$"))
-      (delete-line)
-      ;; Don't warn if we are at the beginning of the buffer
-      (ignore-errors (backward-char))
-      t)
-     ((eolp)
-      ;; On lines with _ only, convert it to an empty line
-      (when (looking-back "^ *[_-]$" (line-beginning-position))
-        (delete-line)
-        (backward-char)
-        (dwim-coder-insert-interactive ?\n))
-      (dwim-coder-insert-interactive ?\n)
-      t)
-     ;; Move up a list if list we contain ends in the same line.
-     ;; Do a regex match first as `up-list' can be very slow if list is big
-     ;; fixme: Use a better approach
-     ((and (looking-at-p ".*[])}].")
-           (setq value (save-excursion
-                         (ignore-errors (up-list))
-                         (point)))
-           (> value (point))
-           (< value (line-end-position)))
-      (up-list)
-      t)
-     (t
-      (end-of-line)
-      t))))
-
 (defun dwim-coder-default-override-self-insert (char)
   (pcase char
     ((guard (dwim-coder-default-be-sane) nil))
-    (?\; (dwim-coder-default-dwim-semi-colon))
+    (?\; (dwim-coder-common-dwim-semi-colon))
     ;; skip in strings
     ((guard (nth 3 (syntax-ppss)) nil))
     (?\s (dwim-coder-default-dwim-space))
